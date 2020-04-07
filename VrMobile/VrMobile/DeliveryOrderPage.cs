4d88af74cfbd6d2f8ee8e2589e4a4cb8757f0496
enmanuelcruz <enmanuelcruzdejesus@gmail.com>
2020-04-06 21:16:07 -0400

    init.

---
+++ VrMobile/VrMobile/DeliveryOrderPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace VrMobile
+{
+    public class DeliveryOrderPage : ContentPage
+    {
+        public DeliveryOrderPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
