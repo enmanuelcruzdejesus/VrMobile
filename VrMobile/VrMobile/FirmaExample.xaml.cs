aaf3f6fe212db9b3631864feee5e2a3dc38af123
enmanuelcruz <enmanuelcruzdejesus@gmail.com>
2020-04-07 23:55:03 -0400

    init

---
+++ VrMobile/VrMobile/FirmaExample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VrMobile
+{
+    public partial class FirmaExample : ContentPage
+    {
+        public FirmaExample()
+        {
+            InitializeComponent();
+        }
+    }
+}
